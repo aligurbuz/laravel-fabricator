40e4129bbbfea8bbc6f674bb576c435fca020ff5
aligurbuz <galiant781@gmail.com>
2019-05-26 20:41:45 +0000

    manager formatting

--- src/resources/Handler/FabricatorAbstract.php
+++ src/resources/Handler/FabricatorAbstract.php
@@ -18,6 +18,11 @@ abstract class FabricatorAbstract
     protected $files;
 
     /**
+     * @var string
+     */
+    protected $fabricPath;
+
+    /**
      * FabricatorAbstract constructor.
      * @param Application $app
      */
@@ -26,5 +31,17 @@ abstract class FabricatorAbstract
         $this->app = $app;
 
         $this->files = $this->app['files'];
+
+        $this->fabricPath = app_path();
+    }
+
+    /**
+     * get fabric directory path
+     *
+     * @return string
+     */
+    public function getFabricDirectoryPath()
+    {
+        return $this->fabricPath.''.DIRECTORY_SEPARATOR.'Fabric';
     }
 }
--- src/resources/Handler/LaravelFabricatorManager.php
+++ src/resources/Handler/LaravelFabricatorManager.php
@@ -11,6 +11,23 @@ class LaravelFabricatorManager extends FabricatorAbstract implements FactoryMana
      */
     public function generate()
     {
-        return app()->basePath();
+        //first we create the fabric directory.
+        $this->makeSkeletonFabricDirectory();
+
+        return true;
+    }
+
+    /**
+     * generate skeleton fabric directory
+     *
+     * @return void
+     */
+    public function makeSkeletonFabricDirectory()
+    {
+        // after making directory checks by using the files object..
+        // that is registered in laravel container, we create a directory with the same object.
+        if(!$this->files->isDirectory($this->getFabricDirectoryPath())){
+            $this->files->makeDirectory($this->getFabricDirectoryPath());
+        }
     }
 }
