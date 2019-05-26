8f11984b23c3b4d80fb033adca287fb7ce392cd0
aligurbuz <galiant781@gmail.com>
2019-05-26 12:44:49 +0000

    fabricator service provider singleton object has been added

--- src/LaravelFabricatorServiceProvider.php
+++ src/LaravelFabricatorServiceProvider.php
@@ -4,6 +4,7 @@ namespace Fabricator;
 
 use Illuminate\Support\ServiceProvider;
 use Fabricator\Resource\Console\FabricatorCommand;
+use Fabricator\Resource\Handler\LaravelFabricatorManager;
 
 class LaravelFabricatorServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,12 @@ class LaravelFabricatorServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // with the name fabricator abstract,
+        // we save the laravelFabricatorManager class to the container object as a singleton.
+        $this->app->singleton('fabricator',function(){
+            return new LaravelFabricatorManager();
+        });
+
         // with the help of console,
         // we define a class that allows you to create factory models.
         $this->commands([FabricatorCommand::class]);
