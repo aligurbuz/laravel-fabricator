3d759fb00cb0fe7b9c0020bc28642128c24d4b6f
aligurbuz <galiant781@gmail.com>
2019-05-27 14:30:48 +0000

    laravel fabricator manager fix

--- src/resources/Handler/LaravelFabricatorManager.php
+++ src/resources/Handler/LaravelFabricatorManager.php
@@ -25,21 +25,23 @@ class LaravelFabricatorManager extends FabricatorAbstract implements FactoryMana
      *
      * @throws FileNotFoundException
      */
-    protected function generateSkeletonFabricDirectory()
+    public function generateSkeletonFabricDirectory()
     {
         // after making directory checks by using the files object..
         // that is registered in laravel container, we create a directory with the same object.
         if(!$this->files->isDirectory($this->getFabricatorDirectoryPath())){
-            $this->files->makeDirectory($this->getFabricatorDirectoryPath());
+            $makeDirectory = $this->files->makeDirectory($this->getFabricatorDirectoryPath());
 
             // the fabricator manager classes will be created...
             // only once and will serve as the fabricator manager.
-            if(!$this->files->isFile($this->getFabricatorManagerFile())){
+            if($makeDirectory && !$this->files->isFile($this->getFabricatorManagerFile())){
                 $fabricatorManagerStub = $this->files->get($this->getFabricatorManagerInStub());
 
                 //the content of fabricatorManager.stub file will be write fabricatorManager file
-                $this->files->put($this->getFabricatorManagerFile(),$fabricatorManagerStub);
+                return $this->files->put($this->getFabricatorManagerFile(),$fabricatorManagerStub)===false ?:  true;
             }
         }
+
+        return false;
     }
 }
